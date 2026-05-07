45be4b583b0b8b78c9643adc0da74045a398fac7
alcantaragonza <fergonalcantara@umes.edu.gt>
2026-05-07 14:30:54 -0600

    Fix - Puerto en Railway 0.0.0.0

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use((err, req, res, _next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Broker Pedidos Now corriendo en puerto ${PORT}`);
 });
 
